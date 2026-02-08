553fc4ab223f7e3c2761cabe652814e48cb4328e
Danica <danicanikolic299@gmail.com>
2026-02-08 21:14:24 +0100

    petlja za primanje i izvrsavanje zadatka

--- Upravljanje_dronovima/Klijent/Klijent.cs
+++ Upravljanje_dronovima/Klijent/Klijent.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using Klijent.Modeli;
+using Server.Modeli;
+using System;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
-using Klijent.Modeli;
-
 namespace Klijent
 {
     class Klijent
@@ -12,6 +13,8 @@ namespace Klijent
         static async Task Main()
         {
             UdpClient udpClient = new UdpClient();
+            var serverEndpoint = new IPEndPoint(IPAddress.Loopback, 9000);
+
 
             Letelica l = new Letelica
             {
@@ -27,10 +30,46 @@ namespace Klijent
 
             // Šaljemo serveru
             await udpClient.SendAsync(data, data.Length, "127.0.0.1", 9000);
+           /* await udpClient.SendAsync(data, data.Length, serverEndpoint);*/
 
             // Čekamo potvrdu
             var response = await udpClient.ReceiveAsync();
             Console.WriteLine("Server: " + Encoding.UTF8.GetString(response.Buffer));
+
+            Console.WriteLine("Client spreman za zadatke...");
+
+            while (true)
+            {
+                try
+                {
+                    var zadatakMsg = await udpClient.ReceiveAsync();
+                    string zadatakJson = Encoding.UTF8.GetString(zadatakMsg.Buffer);
+
+                    Zadatak? zadatak = JsonSerializer.Deserialize<Zadatak>(zadatakJson);
+
+                    if (zadatak != null)
+                    {
+                        Console.WriteLine($"Primljen zadatak: {zadatak.Tip} na polju ({zadatak.X},{zadatak.Y})");
+
+                        l.Status = StatusLetelice.Zauzeta;
+                        await Task.Delay(2000); 
+                        zadatak.Status = StatusZadatka.Zavrsen;
+                        l.X = zadatak.X;
+                        l.Y = zadatak.Y;
+                        l.Status = StatusLetelice.Slobodna;
+
+                        string jsonZadatak = JsonSerializer.Serialize(zadatak);
+                        byte[] bytes = Encoding.UTF8.GetBytes(jsonZadatak);
+                        await udpClient.SendAsync(bytes, bytes.Length, serverEndpoint);
+
+                        Console.WriteLine($"Zadatak ({zadatak.Tip}) završen i poslat serveru");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Greška u prijemu zadatka: " + ex.Message);
+                }
+            }
         }
     }
 }
