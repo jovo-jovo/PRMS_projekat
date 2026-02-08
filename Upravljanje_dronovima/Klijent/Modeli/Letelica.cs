d654344a517d36db6441612d397e672c6fc1f253
Danica <danicanikolic299@gmail.com>
2026-02-08 19:34:51 +0100

    Letelica.cs

--- Upravljanje_dronovima/Klijent/Modeli/Letelica.cs
+++ Upravljanje_dronovima/Klijent/Modeli/Letelica.cs
@@ -4,9 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Klijent.Modeli
+namespace Upravljanje_Dronovima.Models
 {
+    public enum TipLetelice { Nadzorna, Izvrsna }
+    public enum StatusLetelice { Slobodna, Zauzeta, UKvaru }
+
     public class Letelica
     {
+        public Guid Id { get; set; }
+        public TipLetelice Tip { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public StatusLetelice Status { get; set; }
     }
 }
